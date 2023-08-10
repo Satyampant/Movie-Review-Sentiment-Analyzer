3b0e01bbccabf06718f32187836404ec546df09e
Satyampant <satyampant420@gmail.com>
2023-08-10 18:58:22 +0530

    Files Upload

---
+++ SentimentAnalyzerEngine.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify, render_template
+import joblib
+import pandas as pd
+import datetime
+import os
+
+IMAGE_FOLDER = os.path.join('static', 'img_pool')
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = IMAGE_FOLDER
+# model_path = os.getcwd()+r'\sentimentanalysis\models\model'
+model_path = os.getcwd() + r'\models'
+classifier = joblib.load(model_path + r'\classifier.pkl')
+
+
+def predictfunc(review):
+    prediction = classifier.predict(review)
+    if prediction[0] == 1:
+        sentiment = 'Positive'
+        prob = classifier.predict_proba(review)[:, 1]
+        img_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'happy.gif')
+        emoji = os.path.join(app.config['UPLOAD_FOLDER'], 'Smiling_Emoji.png')
+
+    else:
+        sentiment = 'Negative'
+        prob = classifier.predict_proba(review)[:, 0]
+        img_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'sad.gif')
+        emoji = os.path.join(app.config['UPLOAD_FOLDER'], 'Sad_Emoji.png')
+    return prob, sentiment, img_filename, emoji
+
+
+@app.route('/', methods = ['POST', "GET"])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/sentiment_prediction', methods = ['POST', "GET"])
+def predict():
+    if request.method == 'POST':
+        result = request.form
+        content = request.form['text']
+        review = pd.Series(content)
+        prob, sentiment, img_filename, emoji = predictfunc(review)
+    return render_template("index.html", text=content, sentiment=sentiment, probability=prob, image=img_filename, emoji=emoji)
+
+
+if __name__ == '__main__':
+    #app.run(debug = True,port=5000)
+    app.run()
---
+++ datapreprocessing/datapreprocessing.py
@@ -0,0 +1,56 @@
+from nltk.corpus import stopwords
+from sklearn.base import BaseEstimator, TransformerMixin
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import re
+import contractions
+
+stop_words = stopwords.words('english')
+new_stopwords = ["mario","la","blah","saturday","monday","sunday","morning","evening","friday","would","shall","could","might"]
+stop_words.extend(new_stopwords)
+stop_words.remove("not")
+stop_words=set(stop_words)
+
+# Removing special character
+def remove_special_character(content):
+    return re.sub('\W+',' ', content )#re.sub('\[[^&@#!]]*\]', '', content)
+
+# Removing Url's
+def remove_url(content):
+    return re.sub(r'http\S+', '', content)
+
+# Removing the stopwords from text
+def remove_stopwords(content):
+    clean_data = []
+    for word in content.split():
+        if word.strip().lower() not in stop_words and word.strip().lower().isalpha():
+            clean_data.append(word.strip().lower())
+    return " ".join(clean_data)
+
+def contraction_expansion(content):
+    return contractions.fix(content)
+
+def data_cleaning(content):
+    content = contraction_expansion(content)
+    content = remove_special_character(content)
+    content = remove_url(content)
+    content = remove_stopwords(content)
+    return content
+
+class DataCleaning(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        print("calling --init--")
+    def fit(self, X, y=None):
+        print("Calling fit")
+        return self
+    def transform(self, X, y=None):
+        print("Calling transform")
+        X = X.apply(data_cleaning)
+        return X
+
+# Lemmatization of word
+class LemmaTokenizer(object):
+    def __init__(self):
+        self.wordnetlemma = WordNetLemmatizer()
+    def __call__(self, reviews):
+        return [self.wordnetlemma.lemmatize(word) for word in word_tokenize(reviews)]
